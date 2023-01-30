4c77cd49b3bb1acf9fb8ff48342f02cca5176e27
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2023-01-30 23:34:57 +0530

    Completed home page

--- sushi_/src/components/pages/addItem.js
+++ sushi_/src/components/pages/addItem.js
@@ -1,70 +1,80 @@
 import React from "react";
-import { Button, Badge, Card, button } from "react-bootstrap";
-import { Form, Label, FormGroup, Input } from "reactstrap";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Button,Card } from "react-bootstrap";
 import axios from "axios";
 import "../../card.css";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
 
-const Add = async () => {
-  const [inTime, setInTime] = useState("");
-  const [outTime, setOutTime] = useState("");
-  const [date, setDate] = useState("");
+const Add = async (id, name, description, category, price, qty) => {
+  const result = await axios.post("http://localhost:3001/api/product/addItem?vendorId=63d7f2b25d9ed68f50467557", {
+    name: name,
+    description: description,
 
-  if (inTime === "") {
-    setInTime(new Date().toLocaleTimeString());
-    setDate(new Date().toLocaleDateString());
+  });
+
+  if (result.status == 200) {
+    NotificationManager.success("Your Product Added Succesfully");
+    setTimeout(() => {}, 20000);
+    window.location = "";
   } else {
-    setOutTime(new Date().toLocaleTimeString());
-    const data = {
-      inTime: inTime,
-      outTime: new Date().toLocaleTimeString(),
-      date: date,
-    };
-    try {
-      // const res = await axios.post(
-      //   `http://localhost:5000/api/employee/attendence/${email}`,
-      //   data
-      // );
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
+    NotificationManager.error("Your Product Added Unsuccesfully");
   }
 };
 
 const Home = () => {
+  const{id} = useParams();
   return (
-    <div className="mt-20 ">
+    <div className="mt-20" style={{ marginLeft: "20rem" }}>
       <Card style={{ width: "30rem" }}>
-        <Card.Body className="">
-          <Card.Title>Add Items</Card.Title>
+        <Card.Body>
+          <Card.Title style={{ marginLeft: "13rem" }}>Add Items</Card.Title>
+          <NotificationContainer></NotificationContainer>
           <div className="my-6 mx-9">
             <div class="row">
               <div class="col">
                 <label className="mb-1">Item Name</label>
                 <input
                   type="text"
-                  //   onChange={(e) => setFirstName(e.target.value)}
-                  //   defaultValue ={firstName}
+                  id="name"
+                  name="name"
+                  style={{}}
+                  placeholder="Product Name"
                   class="form-control"
+                  required
                 />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
+
                 <label className="mb-1">Item Description</label>
-                <input
-                  type="text"
-                  //   onChange={(e) => setLastName(e.target.value)}
-                  //   defaultValue ={lastName}
+                <textarea
+                  type="textarea"
+                  id="description"
+                  name="description"
                   class="form-control"
+                  placeholder="About the product"
                 />
               </div>
             </div>
           </div>
 
-          <Button variant="primary" className="my-3 mx-9">
+          <Button
+            variant="primary"
+            className="my-3 mx-9"
+            onClick={() => {
+              const name = document.getElementById("name").value;
+              const description = document.getElementById(
+                "description"
+              ).value;
+
+              if (name == "") {
+                console.log("No content");
+                NotificationManager.warning("Please enter your product name of ");
+              } else {
+                Add(id, name, description);
+              }
+            }}
+          >
             ADD
           </Button>
         </Card.Body>
